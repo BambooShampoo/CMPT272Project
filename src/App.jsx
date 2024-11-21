7755e6849d6c4df63c7d9cc71e23957d57c8878c
Jenna Han <109244947+Beans-InJeans@users.noreply.github.com>
2024-11-20 18:11:52 -0800

    Add styling and placeholders for future components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import mapPlaceholder from './assets/map_placeholder.png';
 
 function App() {
-
   return (
     <>
       <h1 className="title">E-Comm</h1>
+
+      <div className = "map">
+        <img src={mapPlaceholder} alt='Map'></img>
+      </div>
+
+      <section className='list'>
+        <ul className='emergencies'>
+          <li>Emergency 1 Component</li>
+          <li>Emergency 2 Component</li>
+          <li>Emergency 3 Component</li>
+        </ul>
+      </section>
+      
+      {/* Put form component here */}
     </>
   )
 }
