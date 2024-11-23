54b6c358e8990198fdd1905b233cbe26c3f7b736
BambooShampoo <tylernguyenvovn@gmail.com>
2024-11-23 12:00:54 -0800

    added the map, updated the read me

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import './App.css'
-import mapPlaceholder from './assets/map_placeholder.png';
+import React from 'react';
+import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 
 function App() {
   return (
     <>
       <h1 className="title">E-Comm</h1>
 
-      <div className = "map">
-        <img src={mapPlaceholder} alt='Map'></img>
+      <div className="map">
+        <MapContainer center={[49.275923, -122.913254]} zoom={10} style={{ height: '100%', width: '100%'}}>
+          {/* adapted from https://leafletjs.com/examples/quick-start/ */}
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
       </div>
 
-      <section className='list'>
-        <ul className='emergencies'>
+      <section className="list">
+        <ul className="emergencies">
           <li>Emergency 1 Component</li>
           <li>Emergency 2 Component</li>
           <li>Emergency 3 Component</li>
         </ul>
       </section>
-      
+
       {/* Put form component here */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
