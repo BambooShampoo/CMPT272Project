3216b13e1643f11299c553949609a2e26c1261ff
Jenna Han <109244947+Beans-InJeans@users.noreply.github.com>
2024-11-26 12:48:05 -0800

    Create LoginButton component and add to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import "../App.css";
+import LoginButton from "./LoginButton";
 
 const Header = () => {
     return (
-        <header id="header">
-            <h2>E-Comm</h2>
-        </header>
+        <>
+            <header id="header">
+                <h2>E-Comm</h2>
+                <LoginButton />
+            </header>
+        </>
     )
 }
 
---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import React from "react";
+
+function LoginButton() {
+    return(
+        <button>
+            LOGIN
+        </button>
+    );
+}
+
+export default LoginButton;
