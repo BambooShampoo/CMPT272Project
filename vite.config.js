e3a54953cf8db2ff6d688e61a8ee682b1a9b31ae
acey <amd26@sfu.ca>
2024-11-30 03:15:39 -0800

    storing array of emergencies and updating emergency list working

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     watch: {
+//       usePolling: true,
+//       interval: 100, // Adjust polling frequency if needed
+//     },
+//   },
+// })
