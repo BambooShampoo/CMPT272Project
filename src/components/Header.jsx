4904415a60ab2201959684db9942cb5a0c6a9f6d
Jenna Han <109244947+Beans-InJeans@users.noreply.github.com>
2024-11-29 18:28:46 -0800

    Delete login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import "../App.css";
-import LoginButton from "./LoginButton";
 
 const Header = () => {
     return (
         <>
             <header id="header">
                 <h2>E-Comm</h2>
-                <LoginButton />
             </header>
         </>
     )
