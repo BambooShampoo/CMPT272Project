5f291adcb5625e68fe40aef4fc8e906877dc7529
BambooShampoo <tylernguyenvovn@gmail.com>
2024-11-30 13:47:32 -0800

    Can now place multiple markers across whole map

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            {/* <MarkerList/> */}
+            <MarkerList/> 
           </MapContainer>
         </div>
         <Form />
--- src/components/MarkerList.jsx
+++ src/components/MarkerList.jsx
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import { useState, useEffect} from 'react';
 import {Marker} from 'react-leaflet';
 
 function MarkerList(){
-    const emergency = JSON.parse(localStorage.getItem('emergency'));
+    const [items, setItems] = useState([]);
+
+    const fetchItems = () => {
+        const emergencies = localStorage.getItem('emergencies');
+        setItems(emergencies ? JSON.parse(emergencies) : []);
+    }
+
+    useEffect(() => {
+        fetchItems();
+
+        const handleStorageUpdate = () => {
+            console.log('emergencyReported event detected');
+            fetchItems();
+        };
+
+        window.addEventListener('emergencyReported', handleStorageUpdate);
+
+        return () => {
+            window.removeEventListener('emergencyReported', handleStorageUpdate);
+        };
+    }, []);
+
     return (
         <>
-            <Marker position={[emergency.lat,emergency.lon]}>
+            {items.map((emergency) => (
+                <Marker position={[emergency.lat,emergency.lon]}>
                 {/* @Gordon for onClick of this marker,You should probably make a seperate component and jsx to display the stuff for the emergency as a seperate popup*/}
-            </Marker>
+                </Marker>
+            ))}
         </>
     );
 }
