416bba5ad21df296b3808adac53b9e9235803fb7
gna26 <gna26@sfu.ca>
2024-12-02 19:24:15 -0800

    updated details on emergency info to display day and time

--- src/components/EmergencyDetails.jsx
+++ src/components/EmergencyDetails.jsx
@@ -5,6 +5,19 @@ function displayComment(comment) {
     return comment;
 };
 
+function displayDate(time) {
+    var date = '';
+    for (let i = 0; time[i] != 'T'; i++) {
+        date += time[i];
+    }
+    return date;
+};
+
+function displayTime(time) {
+    let timeSubstring = time.split('T')[1];
+    return timeSubstring.substring(0,5);
+};
+
 function EmergencyDetails( {activeMarkerId} ) {
     if (!activeMarkerId) {
         return <p className="emergency-text">No emergency highlighted</p>
@@ -19,7 +32,8 @@ function EmergencyDetails( {activeMarkerId} ) {
             <b>Reported by:</b> {activeMarkerId.name} ({activeMarkerId.phone[0] + activeMarkerId.phone[1] + activeMarkerId.phone[2]}-
             {activeMarkerId.phone[3] + activeMarkerId.phone[4] + activeMarkerId.phone[5]}-{activeMarkerId.phone[6] + activeMarkerId.phone[7] + activeMarkerId.phone[8] + activeMarkerId.phone[9]})<br/>
             <b>Comments: </b> {displayComment(activeMarkerId.comments)}<br/>
-            <b>Time Reported:</b> {activeMarkerId.time}<br/>
+            <b>Date: </b> {displayDate(activeMarkerId.time)}<br/>
+            <b>Time Reported:</b> {displayTime(activeMarkerId.time)}<br/>
             <b>Status: </b> {activeMarkerId.status}
         </p>
         </>
